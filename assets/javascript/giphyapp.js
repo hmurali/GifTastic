cbfe5997a8f8212376942bab92ad6febc008b202
Hari <augten95@gmail.com>
2019-07-18 13:37:08 -0500

    Added onClick listener and ajax call to display flower gifs on the page

--- assets/javascript/giphyapp.js
+++ assets/javascript/giphyapp.js
@@ -6,6 +6,34 @@ for(var i = 0; i < topics.length; i++){
     flowerButton.text(topics[i]);
     $("#flowerButtons").append(flowerButton);
 }
-$(".flower").on("click", function() {
+/* $(".flower").on("click", function() {
     console.log("You clicked " + $(this).attr("data-flower"));
-})
+}); */
+$("button").on("click", function() {
+    console.log("You clicked " + $(this).attr("data-flower"));
+    var flower = $(this).attr("data-flower");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + flower + "&api_key=JTwlcyqSqkaxbDboRvPXe82WHvcXPhvI&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    .then(function(response) {
+        var results = response.data;
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");
+
+            var rating = results[i].rating;
+
+            var p = $("<p>").text("Rating: " + rating);
+
+            var flowerImage = $("<img>");
+            flowerImage.attr("src", results[i].images.fixed_height.url);
+
+            gifDiv.prepend(p);
+            gifDiv.prepend(flowerImage);
+
+            $("#flowerGifs").prepend(gifDiv);
+        }
+    })
+});
