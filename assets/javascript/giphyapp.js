5e2a9da016ff7efc46b06b0626c9d08ffbce2622
Hari <augten95@gmail.com>
2019-07-19 15:38:15 -0500

    moved block of code that displays the flower image to the body of displayFlowerImage function; moved block of code that displays flower data on buttons to the body of renderButtons function; added event listener to dynamically generated button elements

--- assets/javascript/giphyapp.js
+++ assets/javascript/giphyapp.js
@@ -1,46 +1,11 @@
 // Initial array of flowers
 var topics = ["Rose", "Tulip", "Daisy", "Sunflower", "Zinnia", "Daffodil", "Carnation", "Cherry-blossom", "Dahlia", "Peony"];
-for(var i = 0; i < topics.length; i++){
-    var flowerButton = $("<button>");
-    flowerButton.addClass("flower");
-    flowerButton.attr("data-flower", topics[i]);
-    flowerButton.text(topics[i]);
-    $("#flowerButtons").append(flowerButton);
-}
+
 /* $(".flower").on("click", function() {
     console.log("You clicked " + $(this).attr("data-flower"));
 }); */
 // Function for dumping the rating and static image for each button into the div
 function displayFlowerImage() {
-
-}
-
-// Function for displaying flower data
-function renderButtons() {
-
-}
-
-// Function for playing/pausing flower gifs depending on the state of the gif.
-function pausePlayFlowerGifs() {
-    // The attr jQuery method allows us to get or set the value of any attribute in our HTML element
-    var state = $(this).attr("data-state");
-    console.log("current state: " + state);
-    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-    // Then, set the image's data-state to animate
-    // Else set src to the data-still value
-    if(state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        console.log("animated img src: " + $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-        console.log("state is now: " + state);
-    } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        console.log("static img src: " + $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-        console.log("state is now: " + state);
-    }
-}
-$("button").on("click", function() {
     console.log("You clicked " + $(this).attr("data-flower"));
     var flower = $(this).attr("data-flower");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + flower + "&api_key=JTwlcyqSqkaxbDboRvPXe82WHvcXPhvI&limit=10";
@@ -70,7 +35,49 @@ $("button").on("click", function() {
 
             $("#flowerGifs").prepend(gifDiv);
         }
-    })
-});
+    });
+}
 
+// Function for displaying flower data
+function renderButtons() {
+    for(var i = 0; i < topics.length; i++){
+        var flowerButton = $("<button>");
+        flowerButton.addClass("flower");
+        flowerButton.attr("data-flower", topics[i]);
+        flowerButton.text(topics[i]);
+        $("#flowerButtons").append(flowerButton);
+    }
+}
+
+// Function for playing/pausing flower gifs depending on the state of the gif.
+function pausePlayFlowerGifs() {
+    // The attr jQuery method allows us to get or set the value of any attribute in our HTML element
+    var state = $(this).attr("data-state");
+    console.log("current state: " + state);
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if(state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        console.log("animated img src: " + $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+        console.log("state is now: " + state);
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        console.log("static img src: " + $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+        console.log("state is now: " + state);
+    }
+}
+/* $("button").on("click", function() {
+    
+}); */
+
+// Using $(document).on to add event listeners to dynamically generated button elements
+$(document).on("click", ".flower", displayFlowerImage);
+
+// Calling the renderButtons function to display the initial buttons
+renderButtons();
+
+// Using $(document).on to add event listeners to dynamically generated img elements 
 $(document).on("click", ".flowerGiphy", pausePlayFlowerGifs);
